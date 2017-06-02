8bac0a2bc9ee0b152fa178cd32ff3900bfe2d8ea
dolgowmm <dolgowmm@gmail.com>
2017-06-02 22:26:13 +0700

    create method to get numbers from console

--- src/main/java/ru/dolgov/testapp/calc/Calculator.java
+++ src/main/java/ru/dolgov/testapp/calc/Calculator.java
@@ -1,7 +1,71 @@
 package ru.dolgov.testapp.calc;
 
+import java.io.File;
+
 /**
- * Created by Михалыч on 02.06.2017.
+ * @author M. Dolgov
+ *         2.06.2017.
  */
 public class Calculator {
+
+    private static int add(int[] inputNumbers) {
+        if (inputNumbers.length < 2) {
+            throw new ArithmeticException("Неверные входные параметры");
+        }
+        int result = 0;
+        for (int i = 0; i < inputNumbers.length; i++) {
+            result += inputNumbers[i];
+        }
+        return result;
+    }
+
+    private static int mul(int[] inputNumbers) {
+        if (inputNumbers.length < 2) {
+            throw new ArithmeticException("Неверные входные параметры");
+        }
+        int result = 1;
+        for (int i = 0; i < inputNumbers.length; i++) {
+            result *= inputNumbers[i];
+        }
+        return result;
+    }
+
+    private static int mulAndAdd(int[] inputNumbers) throws ArithmeticException {
+        if (inputNumbers.length != 3) {
+            throw new ArithmeticException("Неверные входные параметры");
+        }
+        return inputNumbers[0] * inputNumbers[1] + inputNumbers[2];
+    }
+
+    public static void commandParser(String command) {
+        String[] commandArray = command.split(" ");
+        int[] inputNumbers = new int[commandArray.length - 1];
+        try {
+            for (int i = 0; i < commandArray.length - 1; i++) {
+                inputNumbers[i] = Integer.parseInt(commandArray[i + 1]);
+            }
+            if (commandArray[0].equals("add")) {
+                System.out.println("Ответ: " + add(inputNumbers));
+            } else if (commandArray[0].equals("mul")) {
+                System.out.println("Ответ: " + mul(inputNumbers));
+            } else if (commandArray[0].equals("mulAndAdd")) {
+                System.out.println("Ответ: " + mulAndAdd(inputNumbers));
+            } else {
+                System.out.println("Введена неправильная команда");
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("После команды нужно вводить числа");
+        }
+    }
+
+    public static void readFile(String fileName) {
+        
+    }
+
+    public static void printHelp() {
+        System.out.println("Чтобы сложить все числа введите команду add <числа через пробел>");
+        System.out.println("Чтобы умножить все числа введите команду mul <числа через пробел>");
+        System.out.println("Чтобы умножить первые два числа и прибавить к ним третье введите команду mulAndAdd <числа через пробел>");
+    }
+
 }
--- src/main/java/ru/dolgov/testapp/main/Main.java
+++ src/main/java/ru/dolgov/testapp/main/Main.java
@@ -1,8 +1,25 @@
 package ru.dolgov.testapp.main;
 
+import ru.dolgov.testapp.calc.Calculator;
+
+import java.util.Scanner;
+
 /**
  * @author M. Dolgov
  *         02.06.2017.
  */
 public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        Calculator.printHelp();
+
+        System.out.println("Введите команду, числа и нажмите Enter");
+
+        String command = scanner.nextLine();
+
+        Calculator.commandParser(command);
+
+    }
 }
