31770c54072693cfea34b19ff331c6cdfb3a22b2
dolgowmm <dolgowmm@gmail.com>
2017-06-03 10:26:23 +0700

    Create tests for Calculator class

--- src/test/java/ru/dolgov/testapp/calc/CalculatorAddTest.java
+++ src/test/java/ru/dolgov/testapp/calc/CalculatorAddTest.java
@@ -1,26 +1,41 @@
 package ru.dolgov.testapp.calc;
 
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-
-import java.util.Arrays;
-import java.util.Collection;
+import org.junit.Assert;
+import org.junit.Test;
 
 /**
  * @author M. Dolgov
  *         03.06.2017.
  */
-@RunWith(Parameterized.class)
-public class CalculatorTest {
-    @Parameterized.Parameters
-    public static Collection<Object[]> data() {
-        return Arrays.asList(new Object[][]{
-                {0, 0, 0, 0},
-                {1, 2, 3, 6},
-                {10, 20, 30, 60},
-                
-        })
+public class CalculatorAddTest {
+
+    @Test
+    public void testAdd() {
+        Assert.assertEquals("Ответ: 0", Calculator.commandParser("add 0 0 0"));
     }
 
-    public void test
+    @Test
+    public void testAddWithWrongParameters() {
+        Assert.assertEquals("Неверные входные параметры", Calculator.commandParser("add 2"));
+    }
+
+    @Test
+    public void testMull() {
+        Assert.assertEquals("Ответ: 6", Calculator.commandParser("mul 1 2 3"));
+    }
+
+    @Test
+    public void testMulWithWrongParameters() {
+        Assert.assertEquals("Неверные входные параметры", Calculator.commandParser("mul 1"));
+    }
+
+    @Test
+    public void testMullAndAdd() {
+        Assert.assertEquals("Ответ: 18", Calculator.commandParser("mulAndAdd 3 4 6"));
+    }
+
+    @Test
+    public void testMulAndAddWithWrongParameters() {
+        Assert.assertEquals("Неверные входные параметры", Calculator.commandParser("mulAndAdd 3"));
+    }
 }
